7789a701aac5b18c3e9bc027e0c4d4056cf5ef23
Ismaelcobolink <ismael@4link.app>
2025-03-01 14:39:17 +0100

    cameras and orbit controls

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,59 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0,
+};
+window.addEventListener("mousemove", (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = -(event.clientY / sizes.height - 0.5);
+});
+
+/**
+ * Base
+ */
+// Canvas
 const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
+
+// Sizes
+const sizes = {
+    width: 400,
+    height: 300,
+};
 
 // Scene
 const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const mesh = new THREE.Mesh(geometry, material);
-
+const mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1, 5, 5, 5), new THREE.MeshBasicMaterial({ color: 0xff0000 }));
 scene.add(mesh);
 
 // Camera
-const sizes = {
-    width: 400,
-    height: 300,
-};
-// FOV, Aspect Ratio
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.z = 2;
+camera.lookAt(mesh.position);
 scene.add(camera);
 
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
 // Renderer
-const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+});
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+
+const tick = () => {
+    controls.update();
+
+    // Render
+    renderer.render(scene, camera);
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+};
+
+tick();
