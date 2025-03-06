a9f3dff6ec6287c109dffcfd8ecfc77cf9fec072
Ismaelcobolink <ismael@4link.app>
2025-03-06 19:07:57 +0100

    11-materials

--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,62 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-
-const imageSource = "/textures/door/color.jpg";
-
-/**
- * Textures
- */
-const loadingManager = new THREE.LoadingManager();
-const textureLoader = new THREE.TextureLoader(loadingManager);
-const texture = textureLoader.load(imageSource);
-texture.colorSpace = THREE.SRGBColorSpace;
-// loadingManager.onProgress = (...rest) => {
-//     console.log("On Progress", rest);
-// };
-// loadingManager.onLoad = () => {
-//     console.log("On Load");
-// };
-// centrar la textura al cenmtro para poder rotarla desde el centro
-// y no desde la esquina
-// texture.center.x = 0.5;
-// texture.center.y = 0.5;
-// // rotar 45 grados
-// texture.rotation = Math.PI / 4;
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 /**
  * Base
  */
+// Canvas
 const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
 
-// Sizes
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Sizes
+ */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
 };
-// Scene
-const scene = new THREE.Scene();
 
-// Object
-const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: false, map: texture })
-);
-scene.add(mesh);
+window.addEventListener("resize", () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
 
-// Camera
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.x = 1;
+camera.position.y = 1;
 camera.position.z = 2;
-camera.lookAt(mesh.position);
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 const tick = () => {
+    // Update controls
     controls.update();
 
     // Render
@@ -70,16 +67,3 @@ const tick = () => {
 };
 
 tick();
-
-window.addEventListener("resize", () => {
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
-
-    // Update aspect ratio
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
-
-    // Update renderer size
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
