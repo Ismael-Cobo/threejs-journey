81e9f3eb119cc1016c4e50047923c16418a440d1
Ismael Cobo <icobo@4link.app>
2025-10-21 18:50:01 +0200

    particles end

--- src/main.ts
+++ src/main.ts
@@ -27,23 +27,37 @@ const scene = new THREE.Scene();
 /**
  * Particles
  */
-// const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
+const textureLoader = new THREE.TextureLoader();
+
 const particlesGeometry = new THREE.BufferGeometry();
 const count = 5000;
+// *3 because is formed by x, y and z
 const position = new Float32Array(count * 3);
-
+// *3 because is formed by rgb
+const colors = new Float32Array(count * 3);
 for (let i = 0; i < count * 3; i++) {
     position[i] = (Math.random() - 0.5) * 10;
+    colors[i] = Math.random();
 }
 
 const positionAttribute = new THREE.BufferAttribute(position, 3);
 particlesGeometry.setAttribute("position", positionAttribute);
+particlesGeometry.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
 
+const particleTexture = textureLoader.load("./textures/particles/2.png");
 const particlesMaterial = new THREE.PointsMaterial({
-    size: 0.02,
+    size: 0.1,
+    // color: "#ff88cc",
+    alphaMap: particleTexture,
+    transparent: true,
+    // alphaTest: 0.001,
+    // depthTest: false,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    vertexColors: true,
 });
-const particle = new THREE.Points(particlesGeometry, particlesMaterial);
-scene.add(particle);
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 /**
  * Lights
  */
@@ -74,9 +88,19 @@ controls.enableDamping = true;
  * Animate
  */
 const timer = new THREE.Timer();
+console.log();
 const tick = () => {
     // Timer
     timer.update();
+    const elapsedTime = timer.getElapsed();
+
+    // update particles
+    for (let i = 0; i < count; i++) {
+        const x = particles.geometry.attributes.position.getX(i);
+        particles.geometry.attributes.position.setY(i, Math.sin(x + elapsedTime));
+    }
+
+    particles.geometry.attributes.position.needsUpdate = true;
 
     // Update controls
     controls.update();
